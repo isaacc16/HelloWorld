0720a88b295927746bfcdb6820a221f51288015c
Isaac Cheng <himcheng16@protonmail.com>
2022-04-21 23:20:16 +0800

    added new task

--- HelloWorld/src/main.cpp
+++ HelloWorld/src/main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 using namespace std;
 
+void say_world() {
+	cout << "World" << endl;
+}
+
+void say_hello() {
+	cout << "Hello" << endl;
+	say_world();
+}
+
 
 
 int main() {
 
-	cout << "Hello World" << endl;
+	say_hello();
 
 	return 0;
 }
